e5d8fa559debd9577aa9eecdb8e78de2bd5cd815
Alnyz <katro.coplax@gmail.com>
2019-04-29 11:17:19 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
 from connecting import Connect, config
 from style import ResponseJson, ResponseRpc
 from apis import Search, DetailApk, TrandingApk
+
+__author__ = "Dyseo"
+__author_email__ = "katro.coplax@gmail.com"
+__package_needed__ = ["lxml", "bs4", "grequests"]
+__version__ = "0.1"
 __all__ = [
 	"Search",
 	"DetailApk",
